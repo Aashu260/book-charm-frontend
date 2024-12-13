2df81675350e09230402c3a0877d5263582b2d3a
Aashu260 <aashitadaid26@gmail.com>
2024-12-13 02:19:57 -0700

    recentlyadded books

--- src/components/RecentlyAdded.jsx
+++ src/components/RecentlyAdded.jsx
@@ -22,7 +22,7 @@ const RecentlyAdded = () => {
   return (
     <div className="mt-8 px-4">
       <h4 className="text-3xl text-amber-950 text-center mb-12">
-        Recently Added Books
+        New Arrivals
       </h4>
       <div className="my-20 grid gap-8 mx-auto grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-4">
         {Data &&
