31bf60580f7b0ffdbec88d9e565e9caebae99590
Aashu260 <aashitadaid26@gmail.com>
2024-12-16 21:33:11 -0700

    removing unwanted

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Nav from "./components/Nav";
-import Product from "./pages/Product";
 import About from "./pages/About";
 import SignUp from "./pages/SignUp";
 import LogIn from "./pages/LogIn";
